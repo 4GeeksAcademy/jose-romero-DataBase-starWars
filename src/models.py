60fed08e86c85c8dba44d96bd5f2997d2b0dc7a7
Jose Luis JoeDev <95258755+Jose890403@users.noreply.github.com>
2024-04-23 17:17:59 +0000

    base de datos starWars jose luis

--- src/models.py
+++ src/models.py
@@ -1,29 +1,85 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Date
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class Users(Base):
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    __tablename__ ='users'
+    id_users =Column(Integer, primary_key= True)
+    user_name =Column(String(250))
+    user_lastName = Column(String(250))
+    user_email = Column(String(200))
+    user_gender = Column(String(20))
+    user_password = Column(String(20))
+    user_fecha_suscripcion =Column (Date, nullable=False)
+    id_favorites =Column(Integer, ForeignKey('id_favorites'))
+    
+    
+
+class Favorites (Base):
+    __tablename__='favorites'
+    id_favorites = Column(Integer, primary_key = True)
+    id_users = Column(Integer, ForeignKey('users.id_users'))
+    id_planets = Column(Integer, ForeignKey('planets.id_planets'))
+    id_characters = Column(Integer, ForeignKey('characters.id_characters'))
+    id_vehicles = Column(Integer, ForeignKey('vehicles.id_vehicles'))
+    
+
+class Planets(Base):
+
+    __tablename__ = 'Planets'
+    id_planets = Column(Integer, primary_key = True)
+    planets_name= Column(String(100))
+    planets_terrain =Column(String(100))
+    planets_description =Column(String(300))
+    id_favorites = Column(Integer, ForeignKey('id_favorites'))
+    favorites = relationship(Favorites)
+
+
+class Vehicles(Base):
+
+    __tablename__ = 'Vehicles'
+    id_vehicles = Column(Integer, primary_key = True)
+    vehicles_name= Column(String(100))
+    vehicles_description =Column(String(300))
+    id_favorites = Column(Integer, ForeignKey('id_favorites'))
+    favorites = relationship(Favorites)
+
+class Characters(Base):
+
+    __tablename__ = 'Characters'
+    id_characters = Column(Integer, primary_key = True)
+    characters_name= Column(String(100))
+    characters_type= Column(String(50))
+    characters_description =Column(String(300))
+    id_favorites = Column(Integer, ForeignKey('id_favorites'))
+    id_creatures = Column(Integer, ForeignKey('id_creatures'))
+    id_droids = Column(Integer, ForeignKey('id_droids'))
+    favorites = relationship(Favorites)
+
+class Creatures(Base):
+    __tablename__ = 'Creatures'
+
+    id_creatures =(Column(Integer, primary_key =True))
+    creatures_name =(Column(String(50)))
+    creatures_description =Column(String(300))
+    id_characters = Column(Integer, ForeignKey('id_characters'))
+    Characters = relationship(Characters)
+
+class Droids(Base):
+    __tablename__ ='Droids'
+    id_droids =(Column(Integer, primary_key =True))
+    droids_name =(Column(String(50)))
+    droids_description =Column(String(300))
+    id_characters = Column(Integer, ForeignKey('id_characters'))
+    Characters = relationship(Characters)
+
+
 
     def to_dict(self):
         return {}
